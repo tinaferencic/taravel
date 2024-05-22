6fe140f80279a851410a5e64b205c03d6c609bdf
larajurkovic <148192232+larajurkovic@users.noreply.github.com>
2024-05-22 20:13:38 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:taravel2/screens/welcome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Outfit'),
+    );
+  }
+}
