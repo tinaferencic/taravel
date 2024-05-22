52e6cf019e1feafdabd4305edf9d36095f950826
larajurkovic <148192232+larajurkovic@users.noreply.github.com>
2024-05-22 20:50:57 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:taravel/main.dart';
+import 'package:taravel2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
